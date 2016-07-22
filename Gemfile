dd81ea034f35b80d25a65bbe3cecccd0791be562
Brenton Hughes <brenton.hughes@gmail.com>
2016-07-22 22:43:31 +0000

    Added new place form, simple_form gem

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem 'rails_12factor', group: :production
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 
-## Gemfile for Rails 3+, Sinatra, and Merb
+# Gemfile for Rails 3+, Sinatra, and Merb
 gem 'will_paginate', '~> 3.1.0'
 gem 'will_paginate-bootstrap'
+
+# Use Simple Forms
+gem 'simple_form'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'places#index'
+  resources :places
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,5 +58,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  root 'places#index'
 end
