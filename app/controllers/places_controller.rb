4b1770c05d6c540b8503d9e09bed4f97b2f0e40c
Brenton Hughes <brenton.hughes@gmail.com>
2016-07-25 19:58:40 +0000

    Added edit place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -15,6 +15,10 @@ class PlacesController < ApplicationController
   end
 
   def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
     @place = Place.find(params[:id])
   end
 
