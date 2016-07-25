3ada9952a8e4fcd59b00f842052caad5a074c308
Brenton Hughes <brenton.hughes@gmail.com>
2016-07-25 21:31:43 +0000

    Added validation for new place fields

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,6 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  validates :name, length: { minimum: 3, maximum: 50 }, presence: true
+  validates :address, length: { minimum: 10, maximum: 100 }, presence: true
+  validates :description, length: { minimum: 10, maximum: 500 }, presence: true
 end
