df53742fb25ee14e74d09b07be8a889cdf7fc6b5
Brenton Hughes <brenton.hughes@gmail.com>
2016-07-25 20:52:29 +0000

    Added spacing on new place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.order("created_at ASC").paginate(:page => params[:page], :per_page => 5)
